f1f98e13b2051fecb7bebb83922c9cffaa38621b
Geraldi Kusuma Arnanto <geral.sbi2@gmail.com>
2013-10-15 12:58:25 +0700

    Add ActionBarSherlock

--- SIAPKontraktor/src/main/java/com/parama/android/siapkontraktor/LoginActivity.java
+++ SIAPKontraktor/src/main/java/com/parama/android/siapkontraktor/LoginActivity.java
@@ -4,20 +4,13 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class LoginActivity extends Activity {
+import com.actionbarsherlock.app.SherlockActivity;
+
+public class LoginActivity extends SherlockActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.login, menu);
-        return true;
-    }
-    
 }
